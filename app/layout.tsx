54886910a3258ba56038fafaeedadac4bb2665e1
Luis Esteban Ramírez <leramirezca@gmail.com>
2025-05-24 09:15:45 -0500

    fix typo

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Interpretador de sueños",
+  title: "OniricApp",
   description: "Descubre el significado de tus sueños",
 };
 
